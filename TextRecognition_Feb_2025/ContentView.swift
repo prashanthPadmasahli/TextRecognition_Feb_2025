304e5260226ec624aa3dcec98a60ae02a18b2081
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-02-05 13:19:19 +0530

    Initial Commit

---
+++ TextRecognition_Feb_2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextRecognition_Feb_2025
+//
+//  Created by mac on 05/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextRecognition_Feb_2025/TextRecognition_Feb_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  TextRecognition_Feb_2025App.swift
+//  TextRecognition_Feb_2025
+//
+//  Created by mac on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TextRecognition_Feb_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
